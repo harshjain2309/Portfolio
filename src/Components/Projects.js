b41d10cd7b0f07035b83e92720fc1579673161f0
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-06-14 23:19:45 +0530

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ProjectBox from './ProjectBox';
-import YogiCareImage from '../images/yogicare.front.jpg';
-import OIPimage from '../images/OIP.front.png';
-import temptrekImage from '../images/TempTrek.front.png';
-import nqueenImage from '../images/nqueen.front.png';
+import YogiCareImage from '../images/p1.png';
+import OIPimage from '../images/p2.png';
+import temptrekImage from '../images/p3.png';
+import nqueenImage from '../images/p4.png';
 
 
 const Projects = () => {
@@ -12,10 +12,10 @@ const Projects = () => {
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
        
-        <ProjectBox projectPhoto={YogiCareImage} projectName="LangSQL-Assistant (Generative AI)" />
-        <ProjectBox projectPhoto={OIPimage} projectName="Multi-Disease Medical Image Classification using CNNs (Deep Learning)" />
-        <ProjectBox projectPhoto={temptrekImage} projectName="Student Performance Indicator(Classical ML)" />
-        <ProjectBox projectPhoto={nqueenImage} projectName="StockNewsBot" />
+        <ProjectBox projectPhoto={p1} projectName="LangSQL-Assistant (Generative AI)" />
+        <ProjectBox projectPhoto={p2} projectName="Multi-Disease Medical Image Classification using CNNs (Deep Learning)" />
+        <ProjectBox projectPhoto={p3} projectName="Student Performance Indicator(Classical ML)" />
+        <ProjectBox projectPhoto={p4} projectName="StockNewsBot" />
       </div>
 
     </div>
