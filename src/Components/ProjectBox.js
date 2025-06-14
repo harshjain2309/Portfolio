9bc873aa983bd7cb51b6b00c84217a501df4e958
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-06-14 23:11:30 +0530

    Update ProjectBox.js

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -9,15 +9,15 @@ const ProjectBox = ({projectPhoto, projectName}) => {
     YogiCareWebsite: "https://yogicare.netlify.app/",
 
     TempTrek: "A user-friendly weather application that provides real-time weather updates, forecasts, and detailed weather information for any location worldwide.",
-    TempTrekGithub: "https://github.com/harshjain2309/TempTrek",
+    TempTrekGithub: "https://github.com/harshjain2309/Medical-Disease-Prediction-Using-CNN",
     TempTrekWebsite: "https://shiny-nasturtium-33876d.netlify.app/",
 
     CampusCollaborate: "This project was developed as part of the Smart India Hackathon 2023 (SIH 2023) under the Government of Jharkhand. Our team, Atomic Technigeers, created a robust web portal that integrates and manages projects taken up by students across various universities and colleges.",
-    CampusCollaborateGithub: "https://github.com/harshjain2309/Online-Integrated-Platform-for-Student-Projects",
+    CampusCollaborateGithub: "https://github.com/harshjain2309/Student-Performance-Indicator",
     CampusCollaborateWebsite: "https://voluble-douhua-db4077.netlify.app/",
 
     NQueenVisualizer: "The N-Queen problem, a classic combinatorial problem in which N queens must be placed on an N×N chessboard such that no two queens can share the same row, column, or diagonal.",
-    NQueenVisualizerGithub: "https://github.com/harshjain2309/N-Queen-visualizer",
+    NQueenVisualizerGithub: "https://github.com/harshjain2309/StockNewsBot",
     NQueenVisualizerWebsite: "https://n-queen-visualizerr.netlify.app/"
   };
 
