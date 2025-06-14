0ae1b6ed5120d7ff123ccde2ccc5a24c2c3c1c24
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-06-14 13:44:38 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,9 +12,8 @@ const About = () => {
           <h1 className='AboutTextHeading' >Get to <b>know</b> me!</h1>
           <p>
           Hi, my name is <b>Harsh Jain</b> and I am from Bhopal, Madhya Pradesh, India. <br/><br/>
-          I'm a <b>MERN stack web developer</b> and a pre-final year college student pursuing <b>BTech in ECE</b>. <br/><br/>
-          I love to learn new technologies; currently, I can create decent web applications and am planning to learn <b>machine learning</b> and <b>generative AI</b> in the near future. <br/><br/>
-          I am open to new <b>collaborations</b> or <b>work opportunities</b> where I can contribute and grow. Feel free to connect with me; links are below.
+          I’m an <b>aspiring Data Scientist</b> and <b>Software Developer</b> and a final year college student pursuing <b>BTech in ECE</b>. <br/><br/>
+           with hands-on experience building classical ML models, deep learning pipelines, and generative AI systems. I enjoy deploying these solutions as interactive web apps using Streamlit and Flask.
 
           </p>
         </div>
