3f73e2d753400aac654d5beaa8e6691ddf21dd12
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-06-14 23:33:26 +0530

    Update ProjectBox.js

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -3,17 +3,17 @@ import { FaGithub } from "react-icons/fa";
 
 const ProjectBox = ({ projectPhoto, projectName }) => {
   const desc = {
-    YogiCare: "YogiCare is a wellness platform offering tailored yoga routines to address various health concerns.",
-    YogiCareGithub: "https://github.com/harshjain2309/LangSQL-Assistant",
+    p1: "YogiCare is a wellness platform offering tailored yoga routines to address various health concerns.",
+    LangSQL-Assistant (Generative AI): "https://github.com/harshjain2309/LangSQL-Assistant",
 
-    TempTrek: "A user-friendly weather application that provides real-time weather updates, forecasts, and detailed weather information for any location worldwide.",
-    TempTrekGithub: "https://github.com/harshjain2309/Medical-Disease-Prediction-Using-CNN",
+    p2: "A user-friendly weather application that provides real-time weather updates, forecasts, and detailed weather information for any location worldwide.",
+    Multi-Disease Medical Image Classification using CNNs (Deep Learning): "https://github.com/harshjain2309/Medical-Disease-Prediction-Using-CNN",
 
-    CampusCollaborate: "This project was developed as part of the Smart India Hackathon 2023 (SIH 2023) under the Government of Jharkhand. Our team, Atomic Technigeers, created a robust web portal that integrates and manages projects taken up by students across various universities and colleges.",
-    CampusCollaborateGithub: "https://github.com/harshjain2309/Student-Performance-Indicator",
+    p3: "This project was developed as part of the Smart India Hackathon 2023 (SIH 2023) under the Government of Jharkhand. Our team, Atomic Technigeers, created a robust web portal that integrates and manages projects taken up by students across various universities and colleges.",
+    Student Performance Indicator(Classical ML): "https://github.com/harshjain2309/Student-Performance-Indicator",
 
-    NQueenVisualizer: "The N-Queen problem, a classic combinatorial problem in which N queens must be placed on an N×N chessboard such that no two queens can share the same row, column, or diagonal.",
-    NQueenVisualizerGithub: "https://github.com/harshjain2309/StockNewsBot"
+    p4: "The N-Queen problem, a classic combinatorial problem in which N queens must be placed on an N×N chessboard such that no two queens can share the same row, column, or diagonal.",
+    StockNewsBot: "https://github.com/harshjain2309/StockNewsBot"
   };
 
   const githubLink = desc[projectName + 'Github'];
