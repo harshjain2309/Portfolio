dfc016fae32d659588154dc5d466a5619a90056c
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-06-14 23:44:08 +0530

    Update ProjectBox.js

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -4,19 +4,19 @@ import { FaGithub } from "react-icons/fa";
 const ProjectBox = ({ projectPhoto, projectName }) => {
   const desc = {
     "LangSQL-Assistant (Generative AI)": {
-      description: "YogiCare is a wellness platform offering tailored yoga routines to address various health concerns.",
+      description: "Built a GenAI-powered SQL agent using LangChain and GROQ LLaMA 3 (8B) to translate natural language into accurate SQL queries across both SQLite3 and MySQL databases.",
       github: "https://github.com/harshjain2309/LangSQL-Assistant"
     },
     "Multi-Disease Medical Image Classification using CNNs (Deep Learning)": {
-      description: "A user-friendly weather application that provides real-time weather updates, forecasts, and detailed weather information for any location worldwide.",
+      description: "Developed four CNN‑based classifiers for Pneumonia (Chest X‑ray), Breast Cancer (Ultrasound), Lung Cancer (CT scans), and Skin Cancer (Dermatoscopic images) using transfer learning (MobileNetV2, Xception), data augmentation, image preprocessing, and hyperparameter tuning to achieve greater that 90\% accuracy on two of HAM10000, IQ‑OTHNCCD, and Chest X‑ray Pneumonia datasets.",
       github: "https://github.com/harshjain2309/Medical-Disease-Prediction-Using-CNN"
     },
     "Student Performance Indicator(Classical ML)": {
-      description: "This project was developed as part of the Smart India Hackathon 2023 (SIH 2023) under the Government of Jharkhand. Our team, Atomic Technigeers, created a robust web portal that integrates and manages projects taken up by students across various universities and colleges.",
+      description: "Built an end-to-end machine learning pipeline to predict student scores using socio-economic and demographic features by training Linear, Ridge, and Lasso Regression models with a modular scikit-learning pipeline for preprocessing, feature engineering, and evaluation.",
       github: "https://github.com/harshjain2309/Student-Performance-Indicator"
     },
     "StockNewsBot": {
-      description: "The N-Queen problem, a classic combinatorial problem in which N queens must be placed on an N×N chessboard such that no two queens can share the same row, column, or diagonal.",
+      description: "Engineered an end-to-end LLM-based chatbot that performs semantic financial research by parsing real-time news URLs and executing context-aware Q\&A via Groq’s ultra-fast Llama3-8b-8192 large language model.",
       github: "https://github.com/harshjain2309/StockNewsBot"
     }
   };
