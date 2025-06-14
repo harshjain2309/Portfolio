6d737ec869a1b4df6ecbdcb6ca2545bb9976c278
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-06-14 14:08:41 +0530

    Update Skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,32 +1,49 @@
-import React from 'react'; 
+// skills.js
+import React from 'react';
 import { CgCPlusPlus } from "react-icons/cg";
-import {FaReact, FaPython, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap} from "react-icons/fa";
-import {DiNodejs} from "react-icons/di";
-import {SiExpress, SiMongodb, SiPostman, SiVercel} from "react-icons/si";
-import { SiJavascript } from "react-icons/si";
+import { FaReact, FaPython, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap,
+         FaChartBar, FaChartLine, FaDatabase, FaCamera } from "react-icons/fa";
+import { DiNodejs } from "react-icons/di";
+import { SiExpress, SiMongodb, SiPostman, SiVercel,
+         SiJavascript, SiStreamlit, SiFlask, SiJupyter, SiGooglecolab, SiPytorch } from "react-icons/si";
 
-const Skills = ({skill}) => {
-    const icon = {
-        'C++': <CgCPlusPlus size={40} color="yellow"/>,
-        Postman: <SiPostman size={40} color="yellow"/>,
-        React: <FaReact size={40} color="yellow"/>,
-        Javascript: <SiJavascript size={40} color="yellow"/>,
-        Node : <DiNodejs size={40} color="yellow"/>,
-        Express : <SiExpress size={40} color="yellow"/>,
-        MongoDb : <SiMongodb size={40} color="yellow"/>,
-        Git : <FaGitAlt size={40} color="yellow"/>,
-        Github : <FaGithub size={40} color="yellow"/>,
-        Npm : <FaNpm size={40} color="yellow"/>,
-        Figma : <FaFigma size={40} color="yellow"/>,
-        Bootstrap: <FaBootstrap size={40} color="yellow"/>,
-        Vercel : <SiVercel size={40} color="yellow"/>
-    }
-    
+const Skills = ({ skill }) => {
+  const icon = {
+    "C++": <CgCPlusPlus size={40} color="yellow" />,
+    "Python": <FaPython size={40} color="yellow" />,
+    "Pandas": <FaChartBar size={40} color="yellow" />,
+    "NumPy": <FaChartBar size={40} color="yellow" />,
+    "Matplotlib": <FaChartBar size={40} color="yellow" />,
+    "Seaborn": <FaChartLine size={40} color="yellow" />,
+    "Scikit-learn": <FaChartBar size={40} color="yellow" />,
+    "TensorFlow": <FaChartLine size={40} color="yellow" />,
+    "Keras": <FaChartLine size={40} color="yellow" />,
+    "PyTorch": <SiPytorch size={40} color="yellow" />,
+    "OpenCV": <FaCamera size={40} color="yellow" />,
+    "Flask": <SiFlask size={40} color="yellow" />,
+    "Streamlit": <SiStreamlit size={40} color="yellow" />,
+    "SQL": <FaDatabase size={40} color="yellow" />,
+    "Git": <FaGitAlt size={40} color="yellow" />,
+    "GitHub": <FaGithub size={40} color="yellow" />,
+    "Google Colab": <SiGooglecolab size={40} color="yellow" />,
+    "Jupyter Notebook": <SiJupyter size={40} color="yellow" />,
+    "Postman": <SiPostman size={40} color="yellow" />,
+    "React": <FaReact size={40} color="yellow" />,
+    "Javascript": <SiJavascript size={40} color="yellow" />,
+    "Node": <DiNodejs size={40} color="yellow" />,
+    "Express": <SiExpress size={40} color="yellow" />,
+    "MongoDb": <SiMongodb size={40} color="yellow" />,
+    "Npm": <FaNpm size={40} color="yellow" />,
+    "Figma": <FaFigma size={40} color="yellow" />,
+    "Bootstrap": <FaBootstrap size={40} color="yellow" />,
+    "Vercel": <SiVercel size={40} color="yellow" />
+  };
+
   return (
     <div title={skill} className='SkillBox'>
-      {icon[skill]}
+      {icon[skill] || <span>{skill}</span>}
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
