3c3bc17cdd5a4386ecb7e59337b68cb48131e435
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-06-14 13:51:56 +0530

    Update Typed.js

--- src/Components/Typed.js
+++ src/Components/Typed.js
@@ -6,7 +6,7 @@ const Typed = () => {
     <div className="TypeEffect">
       <Typewriter
         options={{
-          strings: ['Software Developer', 'MERN Stack Developer'],
+          strings: ['Aspiring Data Scientist', 'Aspiring Software Developer'],
           autoStart: true,
           loop: true,
           delay: 70,
